b49e5adab5f2c4ad683413591c50f0d14db6f1e0
Bilbia <biamcs2000@gmail.com>
2022-02-24 19:37:16 -0300

    atualizando steamvr

--- Assets/_Scripts/VR_Teleport.cs
+++ Assets/_Scripts/VR_Teleport.cs
@@ -11,7 +11,7 @@ public class VR_Teleport : MonoBehaviour
         if(col.gameObject.tag == "Player_Head") 
         {
             Debug.Log("COLIDIU");
-            thePlayer.transform.position = new Vector3(0, 20, 0);
+            thePlayer.transform.position = new Vector3(0, 5, 0);
         }
     }
 }
